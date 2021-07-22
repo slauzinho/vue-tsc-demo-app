81848df0fff072ab437c44448b9b7af5c2caea7f
teerryn <slaughterer90@gmail.com>
2021-07-22 09:22:51 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+</template>
+
+<script lang="ts">
+import { defineComponent, Ref, ref } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    const card = ref<HTMLElement | string>('');
+    const logCardInfo = (card: Ref<HTMLElement | string>) => {
+      console.log(card.value);
+    };
+    logCardInfo(card);
+  },
+});
+</script>
